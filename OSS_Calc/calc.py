69ebbf3d85811134390e28ffea825127af4dd412
김가현 <hyeonnni@naver.com>
2025-06-04 08:42:48 +0900

    [기능추가] 제곱 기능 추가

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -19,7 +19,8 @@ class Calculator:
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['='],
+            ['x²']
         ]
 
         for row in buttons:
@@ -42,11 +43,19 @@ class Calculator:
                 self.expression = str(eval(self.expression))
             except Exception:
                 self.expression = "에러"
+        elif char == 'x²':
+            try:
+                value = eval(self.expression)
+                self.expression = str(value ** 2)
+            except Exception:
+                self.expression = "에러"
         else:
             self.expression += str(char)
 
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.expression)
+
+
 
 
 
